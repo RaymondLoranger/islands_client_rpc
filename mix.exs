9f8020d75a5dc2e70118d14ec501168e95ba3df2
RaymondLoranger <Raymond.Loranger@bell.net>
2021-01-18 19:58:27 -0500

    0.1.13

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Client.RPC.MixProject do
   def project do
     [
       app: :islands_client_rpc,
-      version: "0.1.12",
+      version: "0.1.13",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Islands Client RPC",
